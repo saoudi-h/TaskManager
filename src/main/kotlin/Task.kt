eb478bb7fb8be615d670599d8bb60eda3465b25b
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-22 09:31:33 +0200

    create Task

---
+++ src/main/kotlin/Task.kt
@@ -0,0 +1,13 @@
+class Task {
+
+    private var id:Int
+    private var title: String
+    private var isCompleted: Boolean
+
+    init{
+        id = 0
+        title = ""
+        isCompleted = false
+    }
+
+}
